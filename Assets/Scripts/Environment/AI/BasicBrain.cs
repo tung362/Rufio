b0f30e36c904b642d74fc761117a4f642e574c92
A. Connor Adam <connor.adam@ymail.com>
2016-05-25 10:30:59 -0700

    AI Script started

---
+++ Assets/Scripts/Environment/AI/BasicBrain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//  t_ denotes a Timer
+//  s_ denotes a state
+// st_ denotes a statistic
+// ro_ denotes a raw object (not for direct use)
+
+public class BasicBrain : MonoBehaviour
+{
+    /* STATES */
+    private enum s_PlayerAttitude { IDLE, HOSTILE, ALERT }
+    private enum s_Personality { FREIND, NEUTRAL, ENEMY }
+    private bool s_HasSeenPlayer;
+    /* TIMERS */
+    private float t_HasSeenPlayer;
+    /* STATISTICS */
+    public float st_Speed;
+    public float st_Damage;
+
+    /* RAW OBJECTS */
+
+	void Start ()
+    {
+	
+	}
+	
+
+	void Update ()
+    {
+	
+	}
+}
